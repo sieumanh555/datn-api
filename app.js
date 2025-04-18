a9eb7c6a7b9f66a013cfeeba55e9c185c123367d
halo <halo@gmail.com>
2025-04-18 08:28:02 +0700

    up

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(cors({
-  origin: ' http://localhost:3000',
+  origin: 'http://103.20.96.47:3000',
   credentials: true 
 }));
 
