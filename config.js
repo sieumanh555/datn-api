4f15d44b816c8fb23ce0579b9c77fe018a781c91
halo <halo@gmail.com>
2025-05-04 21:18:20 +0700

    up

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ module.exports = {
     secret_key: process.env.SECRET_KEY,
     app_id: process.env.APP_ID,
     key1: process.env.KEY1,
+    end_point_zalo: process.env.END_POINT_ZALOSB,
     key2: process.env.KEY2,
     callback_url: process.env.CALLBACK_URL
 }
